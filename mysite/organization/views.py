afb3f7120a6bd3e74cf033bdac1dcc2abdd116cf
Satyajeet Pal <psatyajeet@gmail.com>
2013-09-07 16:07:38 -0400

    Remove extraneous files

--- mysite/organization/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    context = {}
-
-    return render(request, 'organization/homepage.html', context)
